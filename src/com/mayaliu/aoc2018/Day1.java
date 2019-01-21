47d558bb80025af5061a8d881a443e1189498858
Maya Barrett <maya.ye.liu@gmail.com>
2019-01-20 22:22:43 -0500

    Day 1 parts one and two

--- src/com/mayaliu/aoc2018/Day1.java
+++ src/com/mayaliu/aoc2018/Day1.java
@@ -1,7 +1,18 @@
 package com.mayaliu.aoc2018;
 
 public class Day1 {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
+		if (args.length <= 0) {
+			throw new Exception("No input detected!");
+		}
 		
+		int currentFrequency = 0;
+		
+		for(String value : args) {
+			int intValue = Integer.parseInt(value);
+			currentFrequency += intValue;
+		}
+		
+		System.out.println("The new frequency is " + currentFrequency);
 	}
 }
